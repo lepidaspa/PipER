6935a083cfd5f02b684ad35e608b5746a86432a6
Marco Montanari <marco.montanari@gmail.com>
2012-07-26 19:08:36 +0200

    id

--- model_manager/models.py
+++ model_manager/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class DataModelContainer(models.Model):
     
     def __str__(self):
-        return self.id
+        return str(self.id)
 
 class DataModel(models.Model):
     #type = models.CharField(max_length=50, choices=[('point', 'Point'),(''),()])
