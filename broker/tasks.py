3d96b9387937cedbe7b425e9176a36e046158c33
Marco Montanari <marco.montanari@gmail.com>
2012-07-26 19:59:19 +0200

    updater started

--- broker/tasks.py
+++ broker/tasks.py
@@ -1,9 +1,11 @@
 from celery.task import task
 
 from broker.models import *
-
+from datetime import datetime
+from datetime import timedelta
 
 
 @task()
 def read_proxies():
-    print "ciao"
+    for mrt in MetadataRefreshTime.objects.filter(due>datetime.now(), due < datetime.now()+timedelta(minutes=30)):
+        print "ciao"
