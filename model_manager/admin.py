c0874019cc8ecde0f764e13c296195a6dfbfc20c
Marco Montanari <marco.montanari@gmail.com>
2012-07-26 19:02:50 +0200

    correct imports

--- model_manager/admin.py
+++ model_manager/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.db import models
-from model_manager import models
+
+from model_manager.models import *
 
 admin.site.register(DataModelAttribute)
 admin.site.register(DataModelContainer)
