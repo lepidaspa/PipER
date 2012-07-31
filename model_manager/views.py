ad0895856043e2a4525f63225a4fb40b1aad2eea
Marco Montanari <marco.montanari@gmail.com>
2012-07-31 18:03:01 +0200

    dio

--- model_manager/views.py
+++ model_manager/views.py
@@ -99,7 +99,7 @@ def get_model_secondary (request):
                                             'StartID': 'str',
                                             'EndID': 'str',
                                             'Length': 'int',
-                                            'Type': ['Pubblica Illuminazione', 'TLC', 'Rete Elettrica']],
+                                            'Type': ['Pubblica Illuminazione', 'TLC', 'Rete Elettrica'],
                                             'Availability': 'str',
                                             'CreationDate': 'str',
                                             'LastUpdate': 'str',
