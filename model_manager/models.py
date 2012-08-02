dba917c93f17d794472b89a8651de72f3d9f595c
Marco Montanari <marco.montanari@gmail.com>
2012-08-02 13:30:14 +0200

    corrected the model

--- model_manager/models.py
+++ model_manager/models.py
@@ -22,13 +22,15 @@ class DataModelAttributeTable(models.Model):
     
 class DataModelAttributeValues(models.Model):
     table = models.ForeignKey(DataModelAttributeTable)
-    value = models.TextField()
+    value = models.CharField(max_length = 255)
+    super = models.ForeignKey('DataModelAttributeValues', related_name="children")
     def __str__(self):
         return self.name
 
 
 class DataModelAttributeSemantic(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length = 255)
+    filter = models.CharField(max_length = 255, null=True, blank=True)
     related_table_name = models.ForeignKey(DataModelAttributeTable, null=True, blank=True)
     
     def __str__(self):
