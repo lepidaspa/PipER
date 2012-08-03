fcad3a0191862d2c80bd28348f68de16b8c32d84
Marco Montanari <marco.montanari@gmail.com>
2012-08-03 08:14:06 +0200

    remote

--- broker/management/commands/get_remote.py
+++ broker/management/commands/get_remote.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from broker.models import *
 
+from data.views import *
+
 import json
 import urllib2
 
@@ -10,4 +12,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         p = Proxy.objects.get(id=args[0])
         data = urllib2.urlopen("%sdata/%s" % (p.request.url, p.request.token))
-        
+        dd = data.read()
+        print dd
+        put_data(json.loads(dd))
