9aecc5b322f3d34a123257b8fecefa8706ceb06b
Marco Montanari <marco.montanari@gmail.com>
2012-11-13 22:19:20 +0100

    models

--- model_manager/admin.py
+++ model_manager/admin.py
@@ -4,6 +4,5 @@ from django.db import models
 from model_manager.models import *
 
 admin.site.register(DataModelAttribute)
-admin.site.register(DataModelContainer)
 admin.site.register(DataModel)
 admin.site.register(Infrastructure)
