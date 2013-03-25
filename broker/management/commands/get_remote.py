2f7c3a75a1a0374e9feaca6987f3f57c2837b760
sirmmo <sirmmo@gmail.com>
2013-03-25 12:09:14 +0100

    management commands correction

--- broker/management/commands/get_remote.py
+++ broker/management/commands/get_remote.py
@@ -10,11 +10,21 @@ class Command(BaseCommand):
     help = 'Gets data from the remotes'
 
     def handle(self, *args, **options):
-        p = Proxy.objects.get(id=args[0])
-        url = "%sdata/%s" % (p.request.url, p.request.token)
-	print url	
-	data = urllib2.urlopen(url)
+	todo = []
+	if len(args) > 0 :
+		todo.append(args[0])
+	else:
+		todo.extend(Proxy.objects.all().values_list('id', flat=True))
+	print todo
+	for i in todo:
+	        p = Proxy.objects.get(id=i)
+		uu = p.request.url
+		if uu[-1] != "/":
+			uu = uu+"/"
+        	url = "%sdata/%s" % (uu, p.request.token)
+		print url	
+		data = urllib2.urlopen(url)
 	
-        dd = data.read()
-        #print dd
-        put_data(p,json.loads(dd))
+	        dd = data.read()
+        	#print dd
+	        put_data(p,json.loads(dd))
--- broker/management/commands/pull_remote.py
+++ broker/management/commands/pull_remote.py
@@ -10,12 +10,18 @@ class Command(BaseCommand):
     help = 'Gets data from the remotes'
 
     def handle(self, *args, **options):
-        p = Proxy.objects.get(id=args[0])
-        url = p.request.url
-        if url[-1] != "/":
-            url += "/"
-        tot = "%srefreshremote/%s/" % (url, p.request.token)
-        print tot
-        data = urllib2.urlopen(tot)
-        dd = data.read()
-        print  dd
+	todo = []
+	if len(args) > 0:
+		todo.append(args[0])
+	else:
+		todo.extend(Proxy.objects.all().values_list('id', flat=True))
+	for i in  todo:
+	        p = Proxy.objects.get(id=i)
+        	url = p.request.url
+	        if url[-1] != "/":
+        	    url += "/"
+	        tot = "%srefreshremote/%s/" % (url, p.request.token)
+        	print tot
+	        data = urllib2.urlopen(tot)
+        	dd = data.read()
+	        print  dd
